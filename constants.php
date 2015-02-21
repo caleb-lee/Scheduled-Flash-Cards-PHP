440426979481edd06cc1ef70d9b6f7406b3d4bc3
Caleb Lee <caleb.a.lee@icloud.com>
2015-02-21 00:35:30 -0500

    Defines a class for the main menu view

---
+++ constants.php
@@ -0,0 +1,17 @@
+<?php
+// this file keeps track of constants that are used 
+//	across classes, like POST variable names
+
+// set the page title
+$PAGE_TITLE = "Spaced Repetition Flash Card System";
+
+// page names
+$PAGE_GET_VAR = "page";
+$PAGE_GET_NAME_ADD = "add";
+$PAGE_GET_NAME_REVIEW = "review";
+
+// add post names
+$ADD_POST_VARIABLE_FRONT = "cardFront";
+$ADD_POST_VARIABLE_BACK = "cardBack";
+
+?>
---
+++ view/MainMenuView.php
@@ -0,0 +1,14 @@
+<?php
+/***
+ * MainMenuView class
+ * Author: Caleb Lee
+ *
+ * This class simply displays a menu to go to various portions of the web app.
+ ***/
+require_once(__DIR__ . '/../constants.php');
+
+class MainMenuView {
+
+}
+
+?>
