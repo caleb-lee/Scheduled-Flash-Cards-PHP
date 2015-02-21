92aac0d14d3a6b00c7aa115ea6bdb753db9546bd
Caleb Lee <caleb.a.lee@icloud.com>
2015-02-20 23:11:35 -0500

    Makes index page display title

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$TITLE = "Spaced Repetition Flash Card System"
+
+?>
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<meta charset="UTF-8">
+		<title><?php echo $TITLE; ?></title>
+	</head>
+	<body>
+	</body>
+</html>
---
+++ model/Card.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Card class
+ * Author: Caleb Lee
+ *
+ * This class represents a flash card. It has a front (containing text),
+ * a back (also containing text), an interval (amount of time in seconds between last seen 
+ * and now) and a next see date (calculated by some controller)
+ **/
+
+class Card {
+	public $front = '';
+	public $back = '';
+	public $interval = 0;
+	public $lastSeenDate = '0';
+	public $nextDate = '0';
+}
+
+?>
